65276155ef5e350ddf70a75070d95f0659eaa572
student <student@lab-43-2.cs.put.poznan.pl>
2021-10-26 16:24:01 +0200

    pulls
    meh

--- 04-GIT/Program.java
+++ 04-GIT/Program.java
@@ -1,6 +1,10 @@
 public class Hey(){
+	//Second comment
 	public static void main(String[] args){
+		//First comment for logs
 		System.out.print("That was a nice day\n");
 		System.out.print("Goodbye world!");
+		System.out.print(0 == 1);
+		//Comment for logs #2
 	}
 }
